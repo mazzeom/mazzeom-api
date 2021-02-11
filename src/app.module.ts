64bbd4761652255520295b625bb3f9687eebe22e
fine <dlgmltjr0925@gmail.com>
2021-02-11 19:39:26 +0900

    Remove sample code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot({})],
 })
 export class AppModule {}
