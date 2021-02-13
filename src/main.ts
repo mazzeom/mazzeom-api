9da3fc14e2d32c779db3c55b6c2579eb5e00b1f6
fine <dlgmltjr0925@gmail.com>
2021-02-14 02:11:15 +0900

    Add security libraries

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import * as csurf from 'csurf';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
